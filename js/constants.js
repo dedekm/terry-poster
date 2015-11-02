293f115216e3f2dd3fe811d621a249857cf4a2a5
Martin Dedek <martin.dedek@sinfin.cz>
2015-11-02 01:45:17 +0100

    * caption

--- js/constants.js
+++ js/constants.js
@@ -4,7 +4,7 @@
 
   window.HEIGHT = 120;
 
-  window.DEPTH = 8;
+  window.DEPTH = 6;
 
   window.MATERIAL = new THREE.MeshDepthMaterial({
     side: THREE.DoubleSide
--- src/constants.coffee
+++ src/constants.coffee
@@ -1,4 +1,4 @@
 window.WIDTH = 84
 window.HEIGHT = 120
-window.DEPTH = 8
+window.DEPTH = 6
 window.MATERIAL = new THREE.MeshDepthMaterial({side: THREE.DoubleSide})
